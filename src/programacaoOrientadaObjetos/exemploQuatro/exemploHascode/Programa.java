7673f0e727ac1a71114b498ed5a3f1e5d23c4abc
Diego Maia <diego.maia@montreal.com.br>
2021-03-02 16:31:33 -0300

    Adicionando material sobre Equals, Hashcode, This e Super

---
+++ src/programacaoOrientadaObjetos/exemploQuatro/exemploHascode/Programa.java
@@ -0,0 +1,16 @@
+package programacaoOrientadaObjetos.exemploQuatro.exemploHascode;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Veiculo carro1 = new Carro("Palio", "Fiat", 20000.0);
+        Veiculo carro2 = new Carro("Palio", "Fiat", 20000.0);
+
+        System.out.println(carro1 == carro2); //false
+        System.out.println(carro1.equals(carro2)); //true
+
+
+    }
+
+}
---
+++ src/programacaoOrientadaObjetos/exemploQuatro/exemploThis/Motocicleta.java
@@ -0,0 +1,21 @@
+package programacaoOrientadaObjetos.exemploQuatro.exemploThis;
+
+import programacaoOrientadaObjetos.exemploQuatro.exemploThis.Veiculo;
+
+public class Motocicleta extends Veiculo {
+
+    private String cilindradas;
+
+    public String getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(String cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    public double calculaImposto() {
+        return this.getValorVenal() * 0.03;
+    }
+
+}
