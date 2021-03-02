4ad8b14827b46bf61242970989d5cdef7fb589c6
Diego Maia <diego.maia@montreal.com.br>
2021-03-02 15:24:47 -0300

    Adicionando exemplos de construtores

---
+++ src/programacaoOrientadaObjetos/exemploDois/construtorTres/ExemploInstanciaObjeto.java
@@ -0,0 +1,12 @@
+package programacaoOrientadaObjetos.exemploDois.construtorTres;
+
+public class ExemploInstanciaObjeto {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa("Marco");
+
+        System.out.println(pessoa.getNome());
+    }
+
+}
---
+++ src/programacaoOrientadaObjetos/exemploDois/construtorUm/ExemploInstanciaObjeto.java
@@ -0,0 +1,13 @@
+package programacaoOrientadaObjetos.exemploDois.construtorUm;
+
+public class ExemploInstanciaObjeto {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoa = new Pessoa();
+
+        System.out.println(pessoa.getNome());
+
+    }
+
+}
